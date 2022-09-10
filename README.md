12810cb1a3851d1796dc17e3c15e05502c927415
付建邦 <fujianbang0ops@gmail.com>
2022-09-10 22:10:15 +0800

    basic Cli

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # httptide
 
+[![Rust](https://github.com/fujianbang/httptide/actions/workflows/rust.yml/badge.svg)](https://github.com/fujianbang/httptide/actions/workflows/rust.yml)
+
 HTTP-Tide for Terminal —— modern, user-friendly command-line HTTP client, inspired by [httpie](https://github.com/httpie/httpie).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+#[derive(clap::ValueEnum, Debug, Clone)]
+enum Method {
+    GET,
+    POST
+}
+
+/// HTTP-Tide for Terminal —— modern, user-friendly command-line HTTP client, inspired by [httpie]
+#[derive(Parser, Debug)]
+#[clap(about, long_about = None)]
+struct Cli {
+    /// Name of the person to greet
+    #[clap(short, long, value_enum)]
+    method: Method,
+}
+
 fn main() {
-    println!("Hello, {}!", "httptide");
+    let args = Cli::parse();
+
+    for _ in 0..3 {
+        println!("Hello {:?}", args.method)
+    }
 }
